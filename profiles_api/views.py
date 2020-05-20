7e2a19f795c9cc3ac015a6bd40415d10ea6d6ed4
Archna <archnasukhija1996@gmail.com>
2020-05-20 12:54:41 +0530

    Added Permissions to profile feed

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -10,7 +10,8 @@ from profiles_api import permissions
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -103,6 +104,10 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated 
+    )
 
     def perform_create(self, serializer):
         """Sets the user profile to the logged in user"""
